3803ab1b09237b4ced66fbafe1f355dae305b844
ikaruslab1 <ikarusgdm@gmail.com>
2025-09-07 17:22:32 -0600

    add: ponentes, constancia accordion

--- src/scripts/animation1.js
+++
@@ -1,8 +0,0 @@
-import { createDraggable, utils } from 'animejs';
-
-utils.set('.square', { z: 100 });
-
-createDraggable('.square', {
-    x: { mapTo: 'rotateY' },
-    y: { mapTo: 'z' },
-});
---
+++ src/scripts/gafete.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const credencialContenedor = document.getElementById(
+        "credencial-contenedor",
+    );
+    const credencialInterna =
+        credencialContenedor.querySelector(".relative");
+
+    let isFlipped = false;
+
+    const flipCard = () => {
+        credencialInterna.style.transform = isFlipped
+            ? "rotateY(0deg)"
+            : "rotateY(180deg)";
+        isFlipped = !isFlipped;
+    };
+
+    credencialContenedor.addEventListener("click", flipCard);
+
+});
+
+class AstroGreet extends HTMLElement {
+    constructor() {
+        super();
+
+        // Accede a los dos inputs que queremos modificar
+        const passwordInputs = [
+            document.getElementById("target-input"),
+            document.getElementById("target-input2"),
+        ];
+
+        // Lee la clave del atributo data-clave.
+        const clave = this.dataset.clave;
+        const button = this.querySelector("button");
+
+        button.addEventListener("click", () => {
+            const userKey = prompt(
+                "Introduzca su RFC sin homocalve:",
+            ).toUpperCase();
+
+            if (userKey == clave) {
+                passwordInputs.forEach((passwordInput) => {
+                    if (passwordInput) {
+                        if (passwordInput.type === "password") {
+                            passwordInput.type = "text";
+                        } else {
+                            passwordInput.type = "password";
+                        }
+                    }
+                });
+
+                if (passwordInputs[0].type === "text") {
+                    alert(
+                        "Dato correcto, si quiere ocultar los datos introduzca de nuevo su RFC",
+                    );
+                } else {
+                    alert("Datos ocultos de nuevo.");
+                }
+            } else {
+                alert("Clave incorrecta. Inténtalo de nuevo.");
+            }
+        });
+    }
+}
+
+customElements.define("astro-greet", AstroGreet);
